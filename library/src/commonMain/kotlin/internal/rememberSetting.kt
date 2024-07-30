e9f833a8276d37cf5d2c5547bbc81f1ff0bac7e2
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2024-07-30 20:09:25 +0200

    Add LocalFlowSettingsDispatcher and rememberSetting

---
+++ library/src/commonMain/kotlin/internal/rememberSetting.kt
@@ -0,0 +1,28 @@
+package dev.burnoo.compose.remembersetting.internal
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import com.russhwolf.settings.ExperimentalSettingsApi
+import com.russhwolf.settings.coroutines.toFlowSettings
+import com.russhwolf.settings.set
+import dev.burnoo.compose.remembersetting.LocalFlowSettingsDispatcher
+import dev.burnoo.compose.remembersetting.LocalObservableSettings
+
+@OptIn(ExperimentalSettingsApi::class)
+@Composable
+internal inline fun <reified T> rememberSetting(key: String, defaultValue: T): MutableState<T> {
+    val observableSettings = LocalObservableSettings.current
+    val flowSettingsDispatcher = LocalFlowSettingsDispatcher.current
+    val coroutineScope = rememberCoroutineScope()
+    return remember {
+        val flowSettings = if (flowSettingsDispatcher == null) {
+            observableSettings.toFlowSettings()
+        } else {
+            observableSettings.toFlowSettings(flowSettingsDispatcher)
+        }
+        flowSettings.getStateFlow(coroutineScope, key, defaultValue)
+            .asMutableState(coroutineScope, setValue = { observableSettings[key] = it })
+    }
+}
