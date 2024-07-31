d546526e9cbfcdd3b97ea46033750d4d00ac3497
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2024-07-31 20:52:29 +0200

    Use library in the example project

--- example/composeApp/build.gradle.kts
+++ example/composeApp/build.gradle.kts
@@ -68,6 +68,7 @@ kotlin {
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutines.swing)
         }
     }
 }
--- example/composeApp/src/commonMain/kotlin/App.kt
+++ example/composeApp/src/commonMain/kotlin/App.kt
@@ -1,34 +1,28 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import dev.burnoo.compose.remembersetting.rememberStringSetting
+import dev.burnoo.compose.remembersetting.rememberStringSettingOrNull
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import compose_remember_setting.example.composeapp.generated.resources.Res
-import compose_remember_setting.example.composeapp.generated.resources.compose_multiplatform
+
+private const val KEY = "key"
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        var text by rememberStringSetting(KEY, "Hello world!")
+        val nullableText by rememberStringSettingOrNull(KEY)
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { "Hello Compose!" }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            TextField(text, onValueChange = { text = it })
+            nullableText?.let { Text("Saved value: \"$it\"") }
         }
     }
 }
